03ef96e62bf7f7a7d5f0c2aee7da53d722c8384b
Deepak Kumar Jangir <djangir@adobe.com>
2026-07-06 12:51:06 +0530

    Update to typescript

--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/*
-Copyright 2025 Adobe. All rights reserved.
-Licensed under the Apache License, Version 2.0
-*/
-Object.defineProperty(exports, "__esModule", { value: true });
-module.exports = require('./hook');
